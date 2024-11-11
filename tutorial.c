e3e32a5211a70bbf20568aff8acdaac594b1197f
scapy47 <scapy47>
2024-11-11 23:02:54 +0530

    Data types

--- tutorial.c
+++ tutorial.c
@@ -26,11 +26,44 @@ int main()
         // createding a virable is two step process
 
         int a;  // declaration
-        a = 12; // initialization
+        a = 10; // initialization
         // or combined
         int b = 12;
 
+        // how to display variable with format format specifier
+        printf("a:%i\nb:%i\n", a, b); // there are other format specifiers like %i %d %s for intiger,decimal and string data types
+
         // # Data types
-        
+        int x = 365;       // integer (4 bytes)
+        short xs = 123;    // short or short int (2 bytes)
+        long xl = 100000L; // long or long long int (8 bytes)
+        // two times long because int is log by default
+
+        float y = 3.14;   // floating point number (4 bytes)
+        double yd = 3.14; // float with more precision (8 bytes)
+
+        char z = 'S';                   // single character
+        char massage[] = "hello world"; // string or array of character
+        char ascii = 120;               // number can be stored in char
+
+        printf("As a num %d\n", ascii);  // %d can be used to print number
+        printf("As a char %c\n", ascii); // %c to get string that is based on ascii Standard
+
+        // C IS NOT OBJECT ORIENTED
+        /*
+        so there is no string data type only why to write strings is character array sting
+        and there are no buildin functions to manipulate strings char array stings
+        so there is header file that is used for that <string.h>
+        */
+
+        // ## unsigned and signed
+        /*
+        every data type that stores number in c is signed by default
+        which means that they can store both negative and positive numbers
+        and half devided for negative and positive
+        but unsigned data types can only store natural number or positive number
+        which doubles the range of data type
+        unsigned data types have there own format specifiers
+        */
         return 0;
 }
